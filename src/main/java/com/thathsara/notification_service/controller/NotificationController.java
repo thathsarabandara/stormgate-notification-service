84b96eb515c60d234a13bd120bd44a370654bb3d
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-05-23 05:16:57 +0530

    Fixed the checkstyle error for security config

--- src/main/java/com/thathsara/notification_service/controller/NotificationController.java
+++ src/main/java/com/thathsara/notification_service/controller/NotificationController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.thathsara.notification_service.dtos.AdminNotificationGetResponse;
 import com.thathsara.notification_service.dtos.AdminNotificationListGetResponse;
 import com.thathsara.notification_service.dtos.NotificationCreateRequest;
 import com.thathsara.notification_service.dtos.NotificationCreateResponse;
